f71aafc062806e89f58d49d45971591c99a54e0b
rafiffausta <57317107+rafiffausta@users.noreply.github.com>
2021-04-22 10:19:19 +0700

    initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+# Inputan : data set berisi 100 data yang memiliki 3 kolom yakni, id, pelayanan, dan makanan
+# Outputan : data satu vektor kolom berisi 10 baris angka bernilai integer
+
+# Library
+import pandas as pd
+
+# Membaca file
+def imports():
+    data = pd.read_csv('restoran.csv')
+    return data
+
+# Fuzzifikasi
+def fuzzyfication(qualityServices):
+    if()
+    
+if __name__ == "__main__": 
+    df = imports()
+    print(df)
