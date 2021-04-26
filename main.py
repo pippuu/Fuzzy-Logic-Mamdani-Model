cb14a5a3e3f0c57d5a7d844d1757d19a1cab0fb5
Alfinata <alfinata.sitaba@gmail.com>
2021-04-26 17:35:37 +0700

    Done.

--- main.py
+++ main.py
@@ -4,16 +4,23 @@
 # Library
 import pandas as pd
 import numpy as np
+import xlsxwriter
 
 class Fuzzy:
     def __init__(self, ling, value):
         self.ling = str(ling)
         self.value = float(value)
 
+class Worthy:
+    def __init__(self, idx, value):
+        self.idx = idx
+        self.value = value
+
 # Imports
 def imports():
     # data = pd.read_csv("D:\Alpine\Academic\College\4th Semester\Introduction to AI\Tupros\Tupro 2\default\Tupro-AI-02\restoran.csv")
-    data = pd.read_csv("./default/Tupro-AI-02/restoran.csv")
+    # data = pd.read_csv("./default/Tupro-AI-02/restoran.csv")
+    data = pd.read_csv("restoran.csv")
     return data
 
 # Insert data
@@ -108,61 +115,116 @@ def inference(serv, food):
     a = Fuzzy(eligibility, value)
     return a
 
+# Defuzzificate
+def defuzzificate(low, high):
+    mid = low
+    if (high > mid):
+        mid = high
+    if (mid >= 0.5):
+        mid = 0.5
+    sum = ((10+20+30+40)*low + mid*50 + (60+70+80+90+100)*high)/((4*low)+mid+(5*high)) 
+    return sum
+
+# Selection Sort
+def selectionSort(arr):
+    for i in range(len(arr)):
+        max = i
+        for j in range(i+1, len(arr)):
+            if(arr[max].value < arr[j].value):
+                max = j
+        arr[i], arr[max] = arr[max],  arr[i]
+    return arr   
+
+# export to xlsx
+def Exports(arr):
+    workbook = xlsxwriter.Workbook('peringkat.xlsx')
+    worksheet = workbook.add_worksheet()
+
+    row = 0
+    col = 0
+
+    for i in range(10):
+        worksheet.write(row, col, arr[i])
+        row += 1
+
+    workbook.close()
+ 
 if __name__ == "__main__": 
     df = imports()
     arrId, arrServ, arrFood = insertToArray(df)
+    worthArr = []
+    finalArr = []
     
-    temp_1 = fuzzyServ(arrServ[31])
-    for obj in temp_1:
-        print(obj.ling, obj.value, sep=' ')
+    for bruh in range(len(arrId)):
+        temp_1 = fuzzyServ(arrServ[bruh])
+        for obj in temp_1:
+            print(obj.ling, obj.value, sep=' ')
 
-    temp_2 = fuzzyFood(arrFood[31])
-    for obj in temp_2:
-        print(obj.ling, obj.value, sep=' ')
+        temp_2 = fuzzyFood(arrFood[bruh])
+        for obj in temp_2:
+            print(obj.ling, obj.value, sep=' ')
 
-    print("----------------")
+        print("----------------")
 
-    arrLow = []
-    arrHigh = []
-    for i in range(len(temp_1)):
-        for j in range(len(temp_2)):
-            valueInf = inference(temp_1[i], temp_2[j])
-            if (valueInf.ling == "Low"):
-                arrLow.append(valueInf)
-            elif(valueInf.ling == "High"):
-                arrHigh.append(valueInf)
+        arrLow = []
+        arrHigh = []
+        for i in range(len(temp_1)):
+            for j in range(len(temp_2)):
+                valueInf = inference(temp_1[i], temp_2[j])
+                if (valueInf.ling == "Low"):
+                    arrLow.append(valueInf)
+                elif(valueInf.ling == "High"):
+                    arrHigh.append(valueInf)
 
-    for obj in arrLow:
-        print(obj.ling, obj.value, sep=' ')
-    for obj in arrHigh:
-        print(obj.ling, obj.value, sep=' ')
+        for obj in arrLow:
+            print(obj.ling, obj.value, sep=' ')
+        for obj in arrHigh:
+            print(obj.ling, obj.value, sep=' ')
 
-    print("----------------")
+        print("----------------")
 
-    max = 0
-    if (len(arrLow)>1):
-        for i in range(1, len(arrLow)):
-            if (arrLow[i].value > arrLow[max].value):
-                max = i
-    if (len(arrLow)>0):
-        finalLow = arrLow[max]
+        max = 0
+        if (len(arrLow)>1):
+            for i in range(1, len(arrLow)):
+                if (arrLow[i].value > arrLow[max].value):
+                    max = i
+        if (len(arrLow)>0):
+            finalLow = arrLow[max]
 
-    max = 0
-    if (len(arrHigh)>1):
-        for i in range(1, len(arrHigh)):
-            if (arrHigh[i].value > arrHigh[max].value):
-                max = i
-    if (len(arrHigh)>0):
-        finalHigh = arrHigh[max]
+        max = 0
+        if (len(arrHigh)>1):
+            for i in range(1, len(arrHigh)):
+                if (arrHigh[i].value > arrHigh[max].value):
+                    max = i
+        if (len(arrHigh)>0):
+            finalHigh = arrHigh[max]
 
-    print("----------------")
+        print("----------------")
 
-    if (len(arrLow)>0):
-        print(finalLow.ling, finalLow.value, sep=' ')
-    if (len(arrHigh)>0):
-        print(finalHigh.ling, finalHigh.value, sep=' ')
+        if (len(arrLow)>0):
+            print(finalLow.ling, finalLow.value, sep=' ')
+        if (len(arrHigh)>0):
+            print(finalHigh.ling, finalHigh.value, sep=' ')
+
+        if (len(arrLow)<1):
+            worthValue = defuzzificate(0, finalHigh.value)
+        elif (len(arrHigh)<1):
+            worthValue = defuzzificate(finalLow.value, 0)
+        else:
+            worthValue = defuzzificate(finalLow.value, finalHigh.value)
+        print(worthValue)
+        worthArr.append(Worthy(bruh, worthValue))
+        print("----------------")
+    
+    finalArr = selectionSort(worthArr)
+    for obj in finalArr:
+        print(obj.idx, obj.value, sep=' ')
+
+    veryfinalArr = np.array(finalArr)
+    arrOfIdx = []
+    for obj in veryfinalArr:
+        print(obj.idx, obj.value, sep=' ')
+        arrOfIdx.append(obj.idx)
+    Exports(arrOfIdx)
 
     
-    
-    
-
